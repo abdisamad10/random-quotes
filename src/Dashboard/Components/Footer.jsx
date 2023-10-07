0e0bf0cdc4d76582e941f301e9374b534d701757
Abdisamad <cbabanka@gmail.com>
2023-10-07 15:46:55 +0300

    complete random quotes projects

---
+++ src/Dashboard/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div id='footer'>
+        <span>By</span>
+        <a rel='noopener noreferrer' href='https://github.com/babanka'>
+          😺Abdisamad Moha😺
+        </a>
+    </div>
+  )
+}
+
+export default Footer
